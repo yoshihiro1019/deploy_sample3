1844483177f36fd430090501e7dcc5673890320c
Your Name <you@example.com>
2024-10-25 14:59:20 +0900

    1459

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -58,12 +58,10 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_task
     @task = Task.find(params[:id])
   end
 
-    # Only allow a list of trusted parameters through.
   def task_params
     params.require(:task).permit(:name)
   end
